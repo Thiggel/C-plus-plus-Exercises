5f5062e76592012a93e33bc3db3b0cf3e2f4e9a8
Thijs <jellethijsvanderlaan@gmail.com>
2021-09-26 15:41:36 +0200

    Test :

--- week 1/exercise.7/exercise-7.cc
+++ week 1/exercise.7/exercise-7.cc
@@ -1,20 +1,45 @@
+#include <iostream>
+
+using namespace std;
+
 union S
 {
+	struct
+	{
+        	uint32_t  z : 1;
+
+        	uint32_t a : 3;
+        	uint32_t b : 4;
+        	uint32_t c : 5;
+        	uint32_t d : 4;
+        	uint32_t e : 4;
+        	uint32_t f : 4;
+        	uint32_t g : 4;
+        	uint32_t h : 4;
+        	uint32_t i : 5;
+        	uint32_t j : 2;
+	};
 	size_t value;
 };
 
-struct T
+
+int main()
 {
-	unsigned a : 3;
-	unsigned b : 4;
-	unsigned c : 5;
-	unsigned d : 4;
-	unsigned e : 4;
-	unsigned f : 4;
-	unsigned g : 4;
-	unsigned h : 4;
-	unsigned i : 5;
-	unsigned j : 2;
-};
+	S cool =
+       	{
+		.a = 7,
+		.b = 15,
+		.c = 10,
+		.d = 6,
+		.e = 7,
+		.f = 15,
+		.g = 15,
+		.h = 7,
+		.i = 0,
+		.j = 3
+	};
+		
+	cout << hex << cool.value; 
+}
 
 
