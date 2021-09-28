a3ef9d04ee7f92a5327181503e22415c48d48962
Thijs <jellethijsvanderlaan@gmail.com>
2021-09-28 14:45:37 +0200

    added comments

--- week-2/10/10-Thijs.cc
+++ week-2/10/10-Thijs.cc
@@ -5,19 +5,19 @@ using namespace std;
 
 int main()
 {
-	string characters = "";
+	string characters = "";						// initialize string to add ASCII characters to
 
-	for(char character = ' '; character <= '~'; ++character)
+	for(char character = ' '; character <= '~'; ++character) 	// loop through ASCII characters that are not control codes
 	{
-		if(islower(character))
+		if(islower(character))					// characters that are a lower-case of something are letters 
 		{
-			characters += character;
+			characters += character;			// add this character to our string
 		}
-		else if(isupper(character)) 
+		else if(isupper(character)) 				// characters that are a upper-case of something are letters
 		{
-			characters += character;		
+			characters += character;			// add this character to our string
 		}
 	}
-	cout << characters;
+	cout << characters;						// output final strin
 
 }
--- week-2/9/9.cc
+++ week-2/9/9.cc
@@ -4,10 +4,10 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-	size_t num = atoi(argv[1]);
+	size_t num = atoi(argv[1]);						// extract input number for multiplication table
 
-	for (int i = 1; i <= 10; ++i)
+	for (int i = 1; i <= 10; ++i)						// loop should count from 1 * number until 10 * number
 	{
-		cout << i << " * " << num << " = " << num * i << '\n';
+		cout << i << " * " << num << " = " << num * i << '\n';		// output the table line by line in the format given by exercise	
 	}
 }
