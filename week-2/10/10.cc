cbc83ef71d098d1deae2962bde6b66910c714140
Thijs <jellethijsvanderlaan@gmail.com>
2021-09-30 16:13:33 +0200

    Altered 10 to fit comments. pls check

--- week-2/10/10.cc
+++ week-2/10/10.cc
@@ -1,15 +1,22 @@
+//JB: 0
 #include <iostream>
+#include <ctype.h>
 
 using namespace std;
 
 int main()
 {
-	string ascii_set = "";
+    //JB: SF initialization. Objects (like std::string variables) are default-initialized, well, by default.
+	string characters;							// initialize string to add ASCII characters to
 
-	for (char character = ' '; character <= '~'; ++character)
+
+    //JB: Don't assume anything about the order of letters in the ascii set.
+	for (unsigned char character = 0; character < 128; ++character) 	// ASCII has 127 entries. Loop through all of them
 	{
-		ascii_set += character;
+        //JB: BAK
+		if (islower(character) || isupper(character))			// characters that are a lower-case of something are letters
+			characters += character;				// add this character to our string		
 	}
+	cout << characters;							// output final string
 
-	cout << ascii_set;
 }
