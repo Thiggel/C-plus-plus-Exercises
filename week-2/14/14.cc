5609564055b8c4015fc1d4566d12809d43a02e06
Filipe Laitenberger <f.laitenberger@gmail.com>
2021-10-03 17:46:09 +0200

    14 improved

--- week-2/14/14.cc
+++ week-2/14/14.cc
@@ -1,39 +1,24 @@
 #include <iostream>
-#include <cmath>
 
 using namespace std;
 
+// Hey Jurjen, I hope you're proud :)
+
 int main(int argc, char *argv[])
 {
-	++argv;									// remove the program name
-	--argc;									// decrement argc to account for removed program name
-	
-	for (
-		int combinationIndex = 0; 					// iterate over the number of possible combinations
-		combinationIndex < pow(2, argc); 				// which are 8 (2 to the number of words)
-		++combinationIndex
-	)
-	{
-		string combination = "";					// initialize string for the line (necessary as we
-										// build it from right to left)
-		int sumOfWords = combinationIndex;				// copy the combination index -> subtract numbers from it
-										// without changing original index
-		for (
-			int wordIndex = argc - 1; 				// go from end of argv to beginning
-			combinationIndex && wordIndex >= 0; 
-			--wordIndex
-		)
-		{
-			int binaryIndex = pow(2, wordIndex);			// use 2 to the power of the index
+    ++argv;									                                    // remove the program name
+    --argc;                                                                     // decrement argc to account for removed program name
 
-			if (sumOfWords - binaryIndex >= 0)			// and subtract it from the combination index
-			{
-				string word(argv[wordIndex]);			// convert the argument to a string
-				combination = " " + word + combination;		// append to the left side of the combination
-				sumOfWords -= binaryIndex;			// change the sum (that's why we copied it before)
-			}
-		}
+    for (int combination = 0; combination < (2 << (argc - 1)); ++combination)   // there are 2^argc - 1 combinations
+    {
+        cout << combination << ':';
 
-		cout << combinationIndex << ':' << combination << '\n';		// print the combination and a new line
-	}
+        for (int word = 0; word < argc; ++word)                                 // check for every word if it appears in
+            // the combination
+            if (combination & ((2 << word) / 2))			                    // by seeing if 2^word have ones at the same
+                // position
+                cout << ' ' << argv[word];	                                    // -> print the word
+
+        cout << '\n';
+    }
 }
