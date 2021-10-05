89a6483328a476252011d17ffd5065c0fecb1951
Filipe Laitenberger <f.laitenberger@gmail.com>
2021-10-05 09:08:09 +0200

    .

--- week-2/12/12.cc
+++ week-2/12/12.cc
@@ -1,22 +1,29 @@
 #include <iostream>
+
 using namespace std;
+
 int main()
 {
     string input;
 
-    while(true){
+    while (true)
+    {
         cout << "?";
-        getline(cin,input);
+        getline(cin,input);                              // read input string to be reversed
 
-        if (input.empty())          //Break loop if line is empty
+        if (input.empty())                                      // Break loop if line is empty
             break;
 
-        //Loop for reverse iterator starting from last index in string moving back
-        for (string::reverse_iterator revIt = input.rbegin(); revIt != input.rend(); revIt++)
+
+        for (                                                   // Loop for reverse iterator
+            string::reverse_iterator revIt = input.rbegin();    // starting from last index in
+            revIt != input.rend();                              // string moving back
+            ++revIt
+        )
         {
-            cout << *revIt;
+            cout << *revIt;                                     // and print last digit
         }
+
         cout << '\n';
     }
-    return 0;
 }
