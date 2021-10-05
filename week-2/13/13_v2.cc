8713e4d6d62f8b95d3881c8fbe434abfbe279cb5
Eric B <e.b.brouwer@hotmail.com>
2021-10-05 09:27:11 +0200

    Add files via upload

---
+++ week-2/13/13_v2.cc
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	int number = stoi(argv[1]);
+	
+	cout << number << " = ";
+
+	string calculation;				// initialize variable to save calculation while
+	                                // printing binary digits
+
+	int remainingBinaryDigits = 32;			// we need to print 32 binary digits
+	                                // -> keep track of how many already printed
+
+	if (number < 0)						// if it's a negative number
+	{
+		int const firstBitNegative = -pow(2, 31);		// take -(2^31)
+		            // -> highest presentable number in 32bits with negative sign
+		cout << "1";					// print 1 because first binary digit is 1
+		--remainingBinaryDigits;			// now we only need to print 31 more binary digits
+
+		calculation += to_string(firstBitNegative);	// add the number to the calculation string
+		calculation += " + ";				// add a plus to the calculation string	
+	
+		number -= firstBitNegative;			// subtract -(2^31) from the number so that it gets positive
+	}
+
+	while (true)						// while loop that runs until it's broken from inside
+	{
+		int const exponent = log2(number);			        // get the exponent to compute the
+		                                                    // nearest power of two to the number
+
+		int const nearestPowerOfTwo = pow(2, exponent);	// get nearest power of two
+
+		while (remainingBinaryDigits > exponent + 1)	// print 0's until the point where a 1
+		                                                // should be printed for the nearest power of two
+		{
+			cout << "0";				                // print 0 (see above)
+			--remainingBinaryDigits;		            // now we need to print one binary digit fewer
+		}
+			
+		cout << "1";					                // print 1 to represent nearest power of two
+		--remainingBinaryDigits;			            // now we need to print one binary digit fewer
+
+        calculation += to_string(nearestPowerOfTwo);	// add the number to the calculation string
+
+		number -= nearestPowerOfTwo;			        // subtract nearest power of two from original number to
+								                        // repeat the same procedure with the remaining number
+
+		if (!number)					// if we're already at 0
+		{
+			while (remainingBinaryDigits > 0)	// print remaining 0's
+			{
+				cout << "0";			// print 0 (see above)
+				--remainingBinaryDigits;	// while decrementing our count of binary digits
+			}
+			break;					// end the loop
+		}
+		calculation += " + ";				// if this is not the end, print a plus sign
+	}
+	cout << " = " << calculation << '\n';			// in the end, also print the calculation and a new line
+}
