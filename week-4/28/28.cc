8be4032957a10f243e2617c53ed943663ca802d0
Filipe Laitenberger <f.laitenberger@gmail.com>
2021-10-08 11:03:35 +0200

    .

--- week-4/28/28.cc
+++ week-4/28/28.cc
@@ -2,6 +2,8 @@
 
 using namespace std;
 
+// TODO I think they always want us to create an internal header file and a header file for each class
+
 class Line
 {
 public:
@@ -12,10 +14,9 @@ public:
 bool Line::getLine(string &input)
 {
     for (char index : input)    //Loop for each character in string
-    {
         if (!isspace(index))
             return true;        //Return true if ws-char is found
-    }
+
     return false;               //Return false otherwise
 }
 
@@ -25,28 +26,22 @@ string Line::next(string &input)
     auto const npos = input.find(' '); //TODO Doesn't work for tab yet
 
     if (npos != string::npos) //Return substring if non-ws is found
-    {
         return subStr = input.substr(npos + 1);
-    }
 
     return subStr;     //Return empty string if no substring is available
 }
 
 
-int main() {
-
+int main()
+{
     string input;
     getline(cin, input);
 
     while (!input.empty())
-    {
         if (Line::getLine(input))
         {
             input = Line::next(input);
 
             cout << input << endl;
         }
-
-    }
-    return 0;
 }
