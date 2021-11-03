de489255f82ead733c48fbcbb0d6641c3f243148
Filipe Laitenberger <f.laitenberger@gmail.com>
2021-11-03 19:40:08 +0100

    .

--- week-6/43/strings/safeat.cc
+++ week-6/43/strings/safeat.cc
@@ -9,7 +9,7 @@ std::string &Strings::safeAt(size_t idx) const
 {
     if (idx >= d_size)
     {
-        cout << "uwhudw" << emptyString;
+        cout << emptyString;
 
         emptyString.clear();
         return emptyString;
