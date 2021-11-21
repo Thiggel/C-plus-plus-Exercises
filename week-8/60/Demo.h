ce447ea6f406b5aa3337dcf28bb8aae798fa6344
thijs <jellethijsvanderlaan@gmail.com>
2021-11-21 19:58:00 +0100

    started 60. Pushing to laptop

---
+++ week-8/60/Demo.h
@@ -0,0 +1,26 @@
+#ifndef DEMO_H
+#define DEMO_H
+// https://www.geeksforgeeks.org/copy-elision-in-c/
+// https://en.wikipedia.org/wiki/Copy_elision
+// https://stackoverflow.com/questions/12953127/what-are-copy-elision-and-return-value-optimization
+class Demo
+{
+
+
+public:
+    Demo(const char* str = "\0") //default constructor
+    {
+        cout << "Constructor called";
+    }
+
+    Demo(const Demo &demo)  //copy constructor
+    {
+        cout << "Copy constructor called";
+    }
+
+    ~Demo()
+    {
+        cout << "Destructor called";
+    }
+};
+#endif
