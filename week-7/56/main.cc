c6101dd9d8cdd4943f36b9362cf7f77da043543c
thijs <jellethijsvanderlaan@gmail.com>
2021-11-23 10:20:36 +0100

    :(

--- week-7/56/main.cc
+++ week-7/56/main.cc
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <fstream>
+#include <cstring>
+
 #include "main.ih"
 
 // A: 00
@@ -11,29 +13,43 @@
 
 int main(int argc, char **argv)
 {
-    string inFileName = "";
-    string outFileName = "";
-    cin >> inFileName;
-    cin >> outFileName;
+    bool binary = false;
+
+    string inFileName,
+           outFileName;
+
+    for (size_t index = 1; index < static_cast<size_t>(argc); ++index)
+    {
+        if (strcmp(argv[index], "-b") == 0)
+            binary = true;
+
+        else if (inFileName.compare("") == 0)
+            inFileName = argv[index];
+
+        else
+            outFileName = argv[index];
+    }
 
     ifstream inputFile;
     ofstream outputFile;
 
     inputFile.open (inFileName);
     outputFile.open (outFileName);
+
     string line;
     while ( getline (inputFile,line) )
     {
         string writeLine;
-        for(char& nucleobase : line) {
-            if (nucleobase == 'A')
-                writeLine.append(00);
-            if (nucleobase == 'T')
-                writeLine.append(01);
-            if (nucleobase == 'G')
-                writeLine.append(10);
-            if (nucleobase == 'C')
-                writeLine.append(11);
+        for(int idx = 0; idx < line.length(); idx++)  // segmentation error
+        {
+            if (line.at(idx) == 'A')
+                writeLine.append("00");
+            if (line.at(idx) == 'T')
+                writeLine.append("01");
+            if (line.at(idx) == 'G')
+                writeLine.append("10");
+            if (line.at(idx) == 'C')
+                writeLine.append("11");
         }
         outputFile << writeLine << '\n';
 
